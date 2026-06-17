b48bdeb245f13b4783ba5b0181344f99ebcac7c5
nabhan426 <nabhanmohamed279@gmail.com>
2026-06-18 04:58:14 +0530

    Updated wedding invitation

--- js/main.js
+++ js/main.js
@@ -24,3 +24,40 @@ const observer = new IntersectionObserver(
 animatedImages.forEach(image => {
     observer.observe(image);
 });
+
+const weddingDate = new Date(
+    "July 26, 2026 00:00:00"
+).getTime();
+
+function updateCountdown(){
+
+    const now = new Date().getTime();
+    const distance = weddingDate - now;
+
+    const days = Math.floor(
+        distance / (1000 * 60 * 60 * 24)
+    );
+
+    const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24))
+        / (1000 * 60 * 60)
+    );
+
+    const minutes = Math.floor(
+        (distance % (1000 * 60 * 60))
+        / (1000 * 60)
+    );
+
+    const seconds = Math.floor(
+        (distance % (1000 * 60))
+        / 1000
+    );
+
+    document.getElementById("days").textContent = days;
+    document.getElementById("hours").textContent = hours;
+    document.getElementById("minutes").textContent = minutes;
+    document.getElementById("seconds").textContent = seconds;
+}
+
+updateCountdown();
+setInterval(updateCountdown, 1000);
